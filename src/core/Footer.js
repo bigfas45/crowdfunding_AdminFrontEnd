92b3591266b8ae4cc201d71fcbfb88b29ca71674
NASD OTC <38135973+bigfas45@users.noreply.github.com>
2020-10-27 08:28:09 +0100

    cookie consent front-end addition

--- src/core/Footer.js
+++ src/core/Footer.js
@@ -2,6 +2,8 @@ import React, { Fragment, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import {contactForm} from './apiCore';
 import swal from "sweetalert";
+import CookieConsent from 'react-cookie-consent';
+
 
 
 const Footer = () => {
@@ -69,75 +71,182 @@ const Footer = () => {
     const footer = () => {
         return (
           <Fragment>
-         <footer class="footer bg-dark space-pt">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-4 col-md-6">
-        <div class="footer-contact-info">
-          <h5 class="text-primary mb-4">About NASD</h5>
-          <p class="text-white mb-4"></p>
-          <ul class="list-unstyled mb-0">
-          <li> <b> <i class="fas fa-map-marker-alt"></i></b><span>9th Floor, UBA House, 57 Marina, Lagos State, Nigeria</span> </li>
-            <li> <b><i class="fas fa-microphone-alt"></i></b><span>+234-902-455-9686</span> </li>
-            <li> <b><i class="fas fa-headset"></i></b><span>info@ventureramp.com.ng</span> </li>
-          </ul>
-        </div>
-      </div>
-     <div class="col-lg-4 col-md-6 mt-4 mt-md-0">
-        <div class="footer-link">
-          <h5 class="text-primary mb-4">Useful links</h5>
-          <ul class="list-unstyled mb-0">
-            <li> <a href="#">FAQs </a> </li>
-            <li> <Link to="/complaintsfrom">Complaint </Link> </li>
-            <li> <a href="#">Fees </a> </li>
-            <li> <a href="#">Security </a> </li>
-            <li> <Link to="#">&nbsp; </Link> </li>
-          </ul>
+            <footer class="footer bg-dark space-pt">
+              <div class="container">
+                <div class="row">
+                  <div class="col-lg-4 col-md-6">
+                    <div class="footer-contact-info">
+                      <h5 class="text-primary mb-4">About NASD</h5>
+                      <p class="text-white mb-4"></p>
+                      <ul class="list-unstyled mb-0">
+                        <li>
+                          {' '}
+                          <b>
+                            {' '}
+                            <i class="fas fa-map-marker-alt"></i>
+                          </b>
+                          <span>
+                            9th Floor, UBA House, 57 Marina, Lagos State,
+                            Nigeria
+                          </span>{' '}
+                        </li>
+                        <li>
+                          {' '}
+                          <b>
+                            <i class="fas fa-microphone-alt"></i>
+                          </b>
+                          <span>+234-902-455-9686</span>{' '}
+                        </li>
+                        <li>
+                          {' '}
+                          <b>
+                            <i class="fas fa-headset"></i>
+                          </b>
+                          <span>info@ventureramp.com.ng</span>{' '}
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div class="col-lg-4 col-md-6 mt-4 mt-md-0">
+                    <div class="footer-link">
+                      <h5 class="text-primary mb-4">Useful links</h5>
+                      <ul class="list-unstyled mb-0">
+                        <li>
+                          {' '}
+                          <a href="#">FAQs </a>{' '}
+                        </li>
+                        <li>
+                          {' '}
+                          <Link to="/complaintsfrom">Complaint </Link>{' '}
+                        </li>
+                        <li>
+                          {' '}
+                          <a href="#">Fees </a>{' '}
+                        </li>
+                        <li>
+                          {' '}
+                          <a href="#">Security </a>{' '}
+                        </li>
+                        <li>
+                          {' '}
+                          <Link to="#">&nbsp; </Link>{' '}
+                        </li>
+                      </ul>
 
-          
-          <ul class="list-unstyled mb-0">
-            <li> <Link to="/riskwarning">Risk Warnings </Link> </li>
-            <li> <Link to="/privatenotice">Privacy Notice</Link> </li>
-            <li> <Link to="/security">Security</Link> </li>
-            <li> <Link to="#">&nbsp; </Link> </li>
-            <li> <Link to="#">&nbsp; </Link> </li>
-          </ul>
-        </div>
-      </div>
+                      <ul class="list-unstyled mb-0">
+                        <li>
+                          {' '}
+                          <Link to="/riskwarning">Risk Warnings </Link>{' '}
+                        </li>
+                        <li>
+                          {' '}
+                          <Link to="/privatenotice">Privacy Notice</Link>{' '}
+                        </li>
+                        <li>
+                          {' '}
+                          <Link to="/security">Security</Link>{' '}
+                        </li>
+                        <li>
+                          {' '}
+                          <Link to="#">&nbsp; </Link>{' '}
+                        </li>
+                        <li>
+                          {' '}
+                          <Link to="#">&nbsp; </Link>{' '}
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
 
-     
-     
-      <div class="col-lg-4 col-md-6 mt-4 mt-lg-0">
-        <h5 class="text-primary mb-4">Subscribe us</h5>
-        <div class="footer-subscribe">
-          <p class="text-white">Sign up to our newsletter to get the latest news and offers.</p>
-          <form>
-            <div class="form-group">
-              <input type="email"    onChange={handleChnage("email")}    value={email} class="form-control" placeholder="Enter email" />
-            </div>
-            <button    onClick={clickSubmit} type="submit" class="btn btn-primary btn-sm">Get notified</button>
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="footer-bottom">
-    <div class="container">
-      <div class="row align-items-center">
-        <div class="col-md-4 text-center text-md-right">
-          <p class="mb-0 text-white"> &copy;Copyright <span id="copyright"> <script>document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))</script></span> <a href="#"> VentureRamp 2020 </a> All Rights Reserved </p>
-        </div>
-        <div class="col-md-4 text-center my-3 mt-md-0 mb-md-0">
-          <a id="back-to-top" class="back-to-top" href="#"><i class="fas fa-angle-double-up"></i> </a>
-        </div>
-        <div class="col-md-4 text-center text-md-right">
-          <p class="mb-0 " style={{color: "white"}}><Link to="/dataprotection"> Data protection Policy</Link> | <Link to="/Privacypolicy"> Privacy Policy</Link> | <Link to="/term_of_use">Terms of Use</Link>
-            </p>
-        </div>
-      </div>
-    </div>
-  </div>
-</footer>
+                  <div class="col-lg-4 col-md-6 mt-4 mt-lg-0">
+                    <h5 class="text-primary mb-4">Subscribe us</h5>
+                    <div class="footer-subscribe">
+                      <p class="text-white">
+                        Sign up to our newsletter to get the latest news and
+                        offers.
+                      </p>
+                      <form>
+                        <div class="form-group">
+                          <input
+                            type="email"
+                            onChange={handleChnage('email')}
+                            value={email}
+                            class="form-control"
+                            placeholder="Enter email"
+                          />
+                        </div>
+                        <button
+                          onClick={clickSubmit}
+                          type="submit"
+                          class="btn btn-primary btn-sm"
+                        >
+                          Get notified
+                        </button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="footer-bottom">
+                <div class="container">
+                  <div class="row align-items-center">
+                    <div class="col-md-4 text-center text-md-right">
+                      <p class="mb-0 text-white">
+                        {' '}
+                        &copy;Copyright{' '}
+                        <span id="copyright">
+                          {' '}
+                          <script>
+                            document.getElementById('copyright').appendChild(document.createTextNode(new
+                            Date().getFullYear()))
+                          </script>
+                        </span>{' '}
+                        <a href="#"> VentureRamp 2020 </a> All Rights Reserved{' '}
+                      </p>
+                    </div>
+                    <div class="col-md-4 text-center my-3 mt-md-0 mb-md-0">
+                      <a id="back-to-top" class="back-to-top" href="#">
+                        <i class="fas fa-angle-double-up"></i>{' '}
+                      </a>
+                    </div>
+                    <div class="col-md-4 text-center text-md-right">
+                      <p class="mb-0 " style={{ color: 'white' }}>
+                        <Link to="/dataprotection">
+                          {' '}
+                          Data protection Policy
+                        </Link>{' '}
+                        | <Link to="/Privacypolicy">
+                          {' '}
+                          Privacy Policy
+                        </Link> | <Link to="/term_of_use">Terms of Use</Link>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <CookieConsent
+                location="bottom"
+                buttonText="Accept"
+                cookieName="JWT_SECRET"
+                style={{ background: '#2B373B' }}
+                buttonStyle={{ color: '#4e503b', fontSize: '13px' }}
+                expires={150}
+              >
+                <p>
+                  We use cookies to enhance your activities and preferences on
+                  our website and to make your visit to the site efficient. By
+                  browsing our website, you consent to our use of cookies. For
+                  more details, please read our
+                </p>
+                <a href="https://nasdng.com/wp-content/uploads/2020/10/NASD-PRIVACY-POLICY.pdf">
+                  Privacy Policy
+                </a>{' '}
+                and{' '}
+                <a href="https://nasdng.com/wp-content/uploads/2020/10/NASD-COOKIES-POLICY.pdf">
+                  Cookie Policy
+                </a>
+              </CookieConsent>
+            </footer>
           </Fragment>
         );
       };
